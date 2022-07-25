edf297c70127fce672beed9fbe19bb1543e28aed
Даниил Ровенский <daniil.rovenskiy@gmail.com>
2022-07-25 16:35:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const questions = [
+	{
+		question: "Давайте начнём с того, какая у Вас цель?",
+		answers: [
+			"Нужен чисто сайт или Лендинг", 
+			"Неважно что, нужны заявки и увеличение продаж", 
+			"Хотите маркетинговую упаковку", 
+			"Интересует таргет и продвижение"
+		],
+		correct: 4,
+	},
+	{
+		question: "Какого рода у Вас бизнес, чтобы понимать какое решение вам нужно? ?",
+		answers: [
+			"Услуги ",
+			"Продажа товаров",
+			"Оба варианта ",
+		],
+		correct: 2,
+	},
+	{
+		question: "Решите ребус и получите бонус (квиз сайт стоимостью 150$)",
+		answers: [
+			"Hypertext Markup Language",
+			"Hypertext Markdown Language",
+			"Hyperloop Machine Language",
+			"Helicopters Terminals Motorboats Lamborginis",
+		],
+		correct: 1,
+	},
+];
